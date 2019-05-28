a25bb90ea7050322cd4019172e545e798bacf07a
Lcla <1585010945@qq.com>
2019-05-28 10:25:10 +0800

    jquery 小功能

---
+++ js/fengzhuang_chajian.js
@@ -0,0 +1,5 @@
+(function($){
+    $.fn.mvPlugin = function(res){
+        console.log(res)
+    };
+})(jQuery);
---
+++ php/validate.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type:text/html;Charset=utf-8');
+// $arr = array(
+// 	"user" => $_GET['loginuser'],
+// 	"pass" => $_GET['loginpass'],
+// 	"name" => 'response'
+ 
+// );
+// echo $_GET['jsoncallback'] . "(".json_encode($arr).")";
+$url='https://m.9ji.com/web/api/floors/specialEnjoy/v1';
+$html = file_get_contents($url);
+echo $html;
+$searchUrl = 'URL?content=';
+if(!empty($_GET['content']))
+{
+    $searchUrl .= $_GET['content'];
+}
+echo file_get_contents($searchUrl);
+?>
